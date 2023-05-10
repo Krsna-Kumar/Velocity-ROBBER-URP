a9ffafb512f5e8a04401d21937532b9ef20d58db
Krishna11 <86921469+Krsna-Kumar@users.noreply.github.com>
2023-05-10 09:53:29 +0530

    Aim Target With Mouse Position Added

---
+++ Assets/Script/AimTarget.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimTarget : MonoBehaviour
+{
+    public float speed = 0.1f; // speed of the lerp
+    public float zPosition = 0f; // the z position of the object
+
+    private Camera mainCamera;
+
+    void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void FixedUpdate()
+    {
+        // Get the mouse position
+        Vector3 mousePosition = Input.mousePosition;
+
+        // Convert the mouse position to world coordinates
+        Vector3 targetPosition = mainCamera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, zPosition));
+
+        // Interpolate the position of the object towards the target position
+        transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
+    }
+}
