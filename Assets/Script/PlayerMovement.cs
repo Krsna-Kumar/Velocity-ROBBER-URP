86f0465a1a008cc7a584beba1478dbd753ea11ac
Krishna11 <86921469+Krsna-Kumar@users.noreply.github.com>
2023-05-16 13:31:43 +0530

    Change Facing of player Added
    Player is Facing in the direction according to horizontal inputs

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
 
+    private Transform playerTransform;
     private Rigidbody playerRb;
 
     private float horizontalInput;
@@ -20,6 +21,16 @@ public class PlayerMovement : MonoBehaviour
         playerRb = GetComponent<Rigidbody>();
     }
 
+
+    private void Update()
+    {
+        //Change facing of player according to horizontal inputs
+        ChangeFacing();
+
+        //Detect collision with Ground and make the player jump
+        Jump();
+    }
+
     private void FixedUpdate()
     {
         //Getting Horizontal input from player and adding velocity to it----Controls - A & D
@@ -28,15 +39,27 @@ public class PlayerMovement : MonoBehaviour
         playerRb.velocity = new Vector3(playerRb.velocity.x, playerRb.velocity.y, horizontalInput * moveSpeed * Time.deltaTime);
     }
 
-    private void Update()
+    private void ChangeFacing()
+    {
+        if(horizontalInput < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if(horizontalInput > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180f, 0);
+        }
+    }
+
+    private void Jump()
     {
         bool isGrounded = Physics.CheckSphere(transform.position, 0.1f, whatIsGround);
 
         verticalInput = Input.GetAxis("Vertical");
 
-        if(verticalInput > 0 && isGrounded)
+        if (verticalInput > 0 && isGrounded)
         {
-            playerRb.AddForce(Vector3.up * jumpForce , ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 }
