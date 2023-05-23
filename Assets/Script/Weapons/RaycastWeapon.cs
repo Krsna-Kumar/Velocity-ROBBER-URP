ab8feb549f9e6a5c37a6049e2c5b1516825d5581
Krishna11 <86921469+Krsna-Kumar@users.noreply.github.com>
2023-05-24 00:38:38 +0530

    HitEffect Done

--- Assets/Script/Weapons/RaycastWeapon.cs
+++ Assets/Script/Weapons/RaycastWeapon.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 public class RaycastWeapon : MonoBehaviour
 {
+    [Header("State")]
     public bool isFiring = false;
+
+    [Header("Weapon Properties")]
+    public ParticleSystem muzzleFlash;
+    public Transform raycastOrigin;
+    public ParticleSystem hitEffect;
+
+    Ray ray;
+    RaycastHit hitinfo;
     public void StartFiring()
     {
         isFiring = true;
+        muzzleFlash.Emit(1);
+
+        //Casting Ray to Detect the collision with other objects
+        ray.origin = raycastOrigin.position;
+        ray.direction = raycastOrigin.forward;
+
+        if(Physics.Raycast(ray, out hitinfo))
+        {
+            //Debug.DrawLine(raycastOrigin.position, hitinfo.point, Color.red, 1.0f);
+
+            hitEffect.transform.position = hitinfo.point;
+            hitEffect.transform.forward = hitinfo.normal;
+            hitEffect.Emit(1);
+        }
+
+
     }
 
     public void StopFiring()
